ed32b8d8b3c4bba2cdf9380e1b1bceb5483f58ba
AwesomeBuilder <building.something.awesome@gmail.com>
2025-08-08 20:53:34 -0700

    Create AudioTranscriberPOCApp.swift

---
+++ AudioTranscriberPOCApp.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// The entry point to the proof‑of‑concept audio transcription app.
+///
+/// This struct conforms to the `App` protocol and hosts a single
+/// `ContentView` in a `WindowGroup`. When the app launches the view
+/// hierarchy defined in `ContentView` is displayed.
+@main
+struct AudioTranscriberPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
